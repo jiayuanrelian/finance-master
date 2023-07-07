7f64c06c613fd2818c19d08adf31c5ea6933f125
JIANGLEI <jianglei@yto.net.cn>
2023-07-07 16:51:59 +0800

    数据库日志

--- simplejob-finance/src/main/java/com/simple/job/finance/utils/CommonUtils.java
+++ simplejob-finance/src/main/java/com/simple/job/finance/utils/CommonUtils.java
@@ -5,7 +5,13 @@ package com.simple.job.finance.utils;
  */
 public class CommonUtils {
 
+    /**
+     * 访问接口地址
+     */
     public static final String URL = "http://api.tushare.pro";
 
+    /**
+     * 密匙
+     */
     public static final String TOKEN = "b9d8623c7dbde160e75147e2b47588810dd8dc3033ec8cadc9f64f53";
 }
